e61add0542fcbf1a658681c579bdb6e9f04f07c3
pratheeshraj <110892110+pratheeshraj@users.noreply.github.com>
2022-09-01 15:04:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function foo(){
+    var res=document.getElementById("main").value;
+    console.log(res);
+    var res=document.getElementById("main1").value;
+    console.log(res);
+    var res=document.getElementById("main2").value;
+    console.log(res);
+    var res=document.getElementById("main3").value;
+    console.log(res);
+}
